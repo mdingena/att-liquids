c0c8e34e162653c477af7e8fb35992e8c1238661
Marc Dingena <marc.dingena@gmail.com>
2023-08-25 16:23:32 +0800

    Create LiquidDefinition.ts

---
+++ src/types/LiquidDefinition.ts
@@ -0,0 +1,11 @@
+export enum LiquidDefinition {
+  DamageBuffPotion = 13764,
+  DamageResistPotion = 19126,
+  FeedReductionDebugPotion = 63318,
+  HealthPotion = 15340,
+  NourishReductionDebugPotion = 51002,
+  Poison = 46018,
+  SelfDamagePotion = 1180,
+  TeleportPotion = 27100,
+  Water = 44872
+}
