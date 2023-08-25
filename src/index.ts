cf18f7f1dd9895b00658a457c90eeaa3416eb837
Marc Dingena <marc.dingena@gmail.com>
2023-08-25 16:23:47 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export default {};
+/* Types */
+export type { EffectDefinition } from './types/EffectDefinition.js';
+export type { LiquidDefinition } from './types/LiquidDefinition.js';
+export type { LiquidVisualChunkDefinition } from './types/LiquidVisualChunkDefinition.js';
+export type { LiquidVisualData } from './types/LiquidVisualData.js';
+
+/* Liquid */
+export { Liquid } from './Liquid.js';
