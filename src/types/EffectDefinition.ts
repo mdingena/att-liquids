5da56faf0ce87714c2837d219eae700463eac21f
Marc Dingena <marc.dingena@gmail.com>
2023-08-25 16:23:29 +0800

    Create EffectDefinition.ts

---
+++ src/types/EffectDefinition.ts
@@ -0,0 +1,14 @@
+export enum EffectDefinition {
+  BuildupTeleportToOverworld = 42574,
+  DamageIndirectEffect = 654,
+  DamageProtectionIndirectEffect = 64858,
+  Feed = 3280,
+  Heal = 48456,
+  Inate = 22728,
+  LuminosityIndirectEffect = 65294,
+  Nourish = 20046,
+  PoisonoverTime = 29250,
+  RevealEffect = 62720,
+  SpeedIndirectEffect = 6764,
+  TeleportToOverworld = 27214
+}
