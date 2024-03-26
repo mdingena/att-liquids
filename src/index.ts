b6df69414ba50415fea2649016cc7a3839f52145
Marc Dingena <marc.dingena@gmail.com>
2024-03-26 14:53:44 +0800

    Export enums as values

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-/* Types */
-export type { EffectDefinition } from './types/EffectDefinition.js';
-export type { LiquidDefinition } from './types/LiquidDefinition.js';
-export type { LiquidVisualChunkDefinition } from './types/LiquidVisualChunkDefinition.js';
-export type { LiquidVisualData } from './types/LiquidVisualData.js';
-
-/* Liquid */
 export { Liquid } from './Liquid.js';
+export { EffectDefinition } from './types/EffectDefinition.js';
+export { LiquidDefinition } from './types/LiquidDefinition.js';
+export { LiquidVisualChunkDefinition } from './types/LiquidVisualChunkDefinition.js';
+export { LiquidVisualData } from './types/LiquidVisualData.js';
